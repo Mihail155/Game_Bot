2af34440fd2e53652b0858ce070f2d7e9439f919
Mihail155 <danilnasirbanov@gmail.com>
2020-10-04 09:56:34 +0300

    Updates

---
+++ precache-manifest.c4dff11728cd880867d7c4c39652c320.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d16736e81ec1379290ba85ee00a778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1047ed0e6372c2394f52",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "80a71913b64530da2fef",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1047ed0e6372c2394f52",
+    "url": "/static/js/2.0bf4ec3b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.0bf4ec3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a71913b64530da2fef",
+    "url": "/static/js/main.e800194f.chunk.js"
+  },
+  {
+    "revision": "ef92bd75b366e40cd562",
+    "url": "/static/js/runtime-main.26a4d653.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
